f6df6603c1710f7e938657e801668ffe188b95f5
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-15 12:34:19 +0530

    feat(notification): orchestrate end-to-end event consumption and real-time delivery

---
+++ src/services/kafka.consumer.ts
@@ -0,0 +1,54 @@
+import {Server} from "socket.io";
+import {consumer} from "../config/kafka.config";
+import {sendNotificationViaSocket} from "../sockets/socket.service";
+import Notification from "../models/Notification";
+
+export const startNotificationConsumer = async (io:Server) => {
+    await consumer.connect();
+
+    await consumer.subscribe({
+        topic: 'activity.created',
+        fromBeginning: true
+    });
+
+    await consumer.run({
+        eachMessage: async ({topic, partition, message}) => {
+            if (!message) return;
+            console.log(`Topic: ${topic} | Partition: ${partition} | Key: ${message.key?.toString()}`);
+
+            try {
+                const rawMessage = message.value!.toString();
+                const payload = JSON.parse(rawMessage);
+
+                const activity = payload.value || payload.data || payload;
+
+                console.log(`📩 New Activity Received: ${activity.verb} by User ${activity.actorId}`);
+
+                const displayMessage = `${activity.actorId} ${activity.verb.toLowerCase()}ed your ${activity.objectType}`;
+
+                const isDelivered = sendNotificationViaSocket(io, activity.recipientId, {
+                    message: displayMessage,
+                    type: activity.verb,
+                    objectId: activity.objectId,
+                    createAt: new Date(),
+                });
+
+                const newNotification = new Notification({
+                    recipientId: activity.recipientId,
+                    actorId: activity.actorId,
+                    type: activity.verb,
+                    message: displayMessage,
+                    targetId: activity.targetId,
+                    isRead: isDelivered,
+                    createAt: new Date(),
+                })
+
+                await newNotification.save();
+                console.log(`✅ Notification stored for user: ${activity.recipientId}`);
+            } catch (e) {
+                console.error('❌ Error processing Kafka message:', e);
+            }
+
+        }
+    });
+}
