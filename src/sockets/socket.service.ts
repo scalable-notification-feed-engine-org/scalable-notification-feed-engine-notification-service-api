d8a0cc42103392b8f15b761da1f4a1e64cd67ec7
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-15 14:14:32 +0530

    feat(socket): upgrade to redis-backed distributed session management

--- src/sockets/socket.service.ts
+++ src/sockets/socket.service.ts
@@ -1,35 +1,39 @@
 import {Server} from "socket.io";
 import {SocketEvents} from "../enums/socket.events";
+import redisClient from '../config/redis.config'
+import {AuthenticatedSocket, socketKeycloakMiddleware} from "../middlewares/auth.middleware";
 
-const onlineUsers = new Map<string,string>();
 
 export const initSocket = (io: Server) =>{
-    io.on('connection', (socket) => {
-        console.log("New client connected to: " + socket.id);
 
-        socket.on(SocketEvents.JOIN, (userId:string) =>{
-            onlineUsers.set(userId, socket.id);
-            console.log(userId, socket.id);
-        });
+    io.use(socketKeycloakMiddleware);
 
-        socket.on('disconnect', () =>{
-            onlineUsers.forEach((value, key)=>{
-               if(value === socket.id){
-                   onlineUsers.delete(key);
-                   console.log(`User ${key} went offline.`);
-               }
-            });
-        })
+    io.on('connection', async (socket: AuthenticatedSocket) => {
+        console.log("New client connected to: " + socket.userId);
+        const userId = socket.userId;
+
+        if(userId){
+            await redisClient.set(`online_user:${userId}`, socket.id);
+
+            socket.on('disconnect', async () => {
+                await redisClient.del(`online_user:${userId}`);
+                console.log(`👋 User ${userId} went Offline`);
+            })
+        }
+
     })
 }
 
-export const sendNotificationViaSocket = (io: Server,userId:string,data:any):boolean =>{
-    const socketId = onlineUsers.get(userId);
+export const sendNotificationViaSocket = async (io: Server,userId:string,data:any):Promise<boolean> =>{
+    const socketId = await redisClient.get(`online_user:${userId}`);
     if(socketId){
         io.to(socketId).emit(SocketEvents.SEND_NOTIFICATION, data);
+        console.log(`⚡ Real-time notification sent to user ${userId}`);
         return true;
+    }else {
+        console.log(`💤 User ${userId} is offline. Saving only to DB.`);
+        return false;
     }
-    return false;
 }
 
 
