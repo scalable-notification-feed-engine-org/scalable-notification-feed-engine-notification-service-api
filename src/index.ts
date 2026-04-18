bdd7abd6448be80416dc8a4bbab143e22adf684e
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-18 16:35:58 +0530

    feat(server): add socket.io Redis adapter for horizontal scaling

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,12 @@ import {Server} from 'socket.io';
 import cors from 'cors';
 import notificationRoutes from './routes/notification.routes';
 import {initSocket} from "./sockets/socket.service";
-import {connectRedis} from "./config/redis.config";
+import redisClient, {connectRedis} from "./config/redis.config";
 import {startNotificationConsumer} from "./services/kafka.consumer";
 import 'dotenv/config'
 import mongoose from "mongoose";
+import * as process from "node:process";
+import {createAdapter} from "@socket.io/redis-adapter";
 
 const app = express()
 app.use(cors());
@@ -21,22 +23,31 @@ const io = new Server(httpServer,{
     cors: {
         origin: '*',
         methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+        credentials: true
     }
 })
 
 initSocket(io);
 
+
+
 const startServer = async () => {
     try {
         await connectRedis();
 
-        await startNotificationConsumer(io);
+      const subClient = redisClient.duplicate();
+        await subClient.connect();
+        io.adapter(createAdapter(redisClient, subClient));
+        console.log(" Socket.io Redis Adapter Connected");
 
         const PORT = process.env.PORT || 5000;
         httpServer.listen(PORT, () => {
             console.log(`🚀 Notification Service running on port ${PORT}`);
         });
 
+         startNotificationConsumer(io).catch(err => {
+             console.error(" Kafka Consumer Failed to Start:", err);
+         });
     }catch (err) {
         // @ts-ignore
         console.error("Critical Server Error:", err.message);
@@ -49,7 +60,7 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI!);
         console.log(`🍃 MongoDB Connected: ${conn.connection.host}`);
     } catch (error: any) {
-        console.error(`❌ Error: ${error.message}`);
+        console.error(` Error: ${error.message}`);
         process.exit(1);
     }
 };
